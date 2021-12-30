2e99f623495b7970d0a3e5c5deecbd19da10721d
Mark Walet <mark.walet@gmail.com>
2021-12-30 13:39:37 +0100

    Fix deprecation warning when sorting version

--- src/Concerns/CanSortReleases.php
+++ src/Concerns/CanSortReleases.php
@@ -23,7 +23,15 @@ trait CanSortReleases
                     return 1;
                 }
 
-                return version_compare($first, $second, '<=');
+                if ($first === $second) {
+                    return 0;
+                }
+
+                if (strtolower($first[0]) === 'v' && strtolower($second[0]) === 'v') {
+                    return version_compare($second, $first);
+                }
+
+                return strcmp($second, $first);
             });
         }
 
